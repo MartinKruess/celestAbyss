d5304b302fea5d1408606e82ff66da3291220048
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2024-05-18 17:28:19 +0200

    add item to inv + stackmanagement

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,32 +1,55 @@
 import { CharDataModel } from "../models/characterSchema.js"
+import { InventoryModel } from "../models/inventorySchema.js";
 import { ItemModel } from "../models/itemSchema.js"
 
 // New Item to Inventory
 const addItem = async (characterID, itemFromDB, amount) => {
+  console.log("Item nicht vorhanden oder nicht Stackbale!")
   await CharDataModel.findandUpdate( { _id: characterID }, {  $push: { inventory: itemFromDB, amount: amount } } )
 }
 
 // Stack Item to Inventory
-const stackItem = async (characterFromDB, itemFromDB, dropAmount) => {
+const stackItem = async (invFromDB, itemFromDB, dropAmount) => {
+  let itemAmount = dropAmount
   
   // Load all current stacks of the item
-  const itemStacks = characterFromDB.inventory.filter(item => item.name === itemFromDB.name)
+  const itemStacks = invFromDB.items.filter(item => item.itemID[0] === itemFromDB._id[0])
 
+  // Fill up existing stacks
   itemStacks.forEach(stack => {
-    const spaceLeft = stack.stacksize - stack.amount
+    const spaceLeft = itemFromDB.stacksize - stack.amount
+    console.log(spaceLeft)
  
     if(spaceLeft > 0){
-      dropAmount <= spaceLeft ? stack.amount += dropAmount : dropAmount = 0
-    } else {
-      stack.amount = itemFromDB.stacksize
-      dropAmount -= spaceLeft
+      if(itemAmount <= spaceLeft) {
+        stack.amount += itemAmount
+        itemAmount = 0
+      } else {
+        stack.amount = itemFromDB.stacksize
+        itemAmount -= spaceLeft
+      }  
     }
   })
+  console.log("Stacks: ", itemStacks)
+  console.log("DropAmount after loop:", itemAmount)
 
-  if(dropAmount > 0){
-    const char = await CharDataModel.findandUpdate( { _id: characterID }, {  $push: { inventory: itemFromDB, amount: dropAmount } } )
-    console.log("Update, new Stack:", char.inventory)
+  // Create new stacks
+  if(itemAmount > 0){
+
+    do {
+      if(itemAmount > itemFromDB.stacksize){
+        invFromDB.items.push({ itemID: itemFromDB._id, amount: itemFromDB.stacksize })
+        itemAmount -= itemFromDB.stacksize
+      } else {
+        invFromDB.items.push({ itemID: itemFromDB._id, amount: itemAmount })
+        itemAmount = 0
+      }
+    } while (itemAmount > 0);
+
+    // save to DB
+    console.log("Update, new Stack:", invFromDB.items)
   }
+
 };
 
 export const updateInventoryByLoot = async (req, res, next) => {
@@ -36,22 +59,23 @@ export const updateInventoryByLoot = async (req, res, next) => {
         const amount = req.body.amount
         const characterID = req.body.characterID
 
-        // Find character by ID
-        const characterFromDB = await CharDataModel.findOne({ _id: characterID })
-        console.log("CharInventoryFromDB", characterFromDB.inventory)
-        
-        const itemFromCharacter = characterFromDB.inventory.find(item => item.itemName === itemName)
-        itemFromCharacter && console.log("ItemFromCharacter: ", itemFromCharacter.name)
-        
-        // Find Item by Name
+        // Find inventory by characterID
+        const inventoryFromDB = await InventoryModel.findOne({characterID: characterID })
+
+        // Find Item by Name in itemDB
         const itemFromDB = await ItemModel.findOne({name: itemName})
-        console.log("ItemFromDB: ", itemFromDB)
 
-        itemFromCharacter && itemFromDB.stacksize > 1
-          ? await stackItem(characterFromDB, itemFromCharacter, amount)
-          : await addItem(characterID, itemFromDB, amount)
+        // Equal?
+        console.log("Equal?", itemFromDB._id[0] === inventoryFromDB.items[0].itemID[0])
 
-    }catch(err){
+        // Item already in Inventory?
+        const itemInInventory = inventoryFromDB.items.find(item => item.itemID[0] === itemFromDB._id[0])
+        
+        itemInInventory && itemFromDB.stacksize > 1
+          ? await stackItem(inventoryFromDB, itemFromDB, amount)
+          : await addItem(inventoryFromDB, itemFromDB, amount)
+
+    } catch (err){
         next(err)
     }
 }
